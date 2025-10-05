7189e03d0359b84d8ca7eacccae3b888c5099616
olifurz <ojreynolds@proton.me>
2025-10-05 01:32:11 +0100

    major changes + blog system

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,6 +1,15 @@
 export default async function(eleventyConfig) {
-    eleventyConfig.addPassthroughCopy("media");
     eleventyConfig.setLayoutsDirectory("../layouts");
     eleventyConfig.setInputDirectory("pages");
     eleventyConfig.setOutputDirectory("dist");
+
+    eleventyConfig.addPassthroughCopy("media");
+    eleventyConfig.addCollection("blog", (collection) =>
+        collection.getFilteredByTag("blog-post").reverse()
+    );
+
+    eleventyConfig.addPassthroughCopy("site.webmanifest");
+    eleventyConfig.addPassthroughCopy(".htaccess");
+
+
 };
