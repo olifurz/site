82493e9061aa86ad5d4e75c0b945322de4eadb7c
olifurz <ojreynolds@proton.me>
2025-10-05 21:45:10 +0100

    app.js & deploy.yml fixes

--- app.js
+++ app.js
@@ -25,7 +25,7 @@ const visitLimiter = rateLimit({
 const db = await mysql.createConnection({
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
+    password: process.env.DB_PASS,
     database: process.env.DB_NAME,
 });
 
