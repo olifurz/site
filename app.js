5e784bad4b60f28fea07112d364eb901fe8362fc
olifurz <ojreynolds@proton.me>
2025-10-06 16:40:07 +0100

    createPool instead of createConnection

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-import express from "express";
+﻿import express from "express";
 import rateLimit from "express-rate-limit";
 import mysql from "mysql2/promise";
 import crypto from "crypto";
@@ -22,11 +22,14 @@ const visitLimiter = rateLimit({
     legacyHeaders: false,
 });
 
-const db = await mysql.createConnection({
+const db = mysql.createPool({
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
     password: process.env.DB_PASS,
     database: process.env.DB_NAME,
+    waitForConnections: true,
+    connectionLimit: 10,
+    queueLimit: 0,
 });
 
 app.post("/api/visit", visitLimiter, async (req, res) => {
